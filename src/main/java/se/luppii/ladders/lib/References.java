c43f8ea717327dcc17c63494240f945d75d04f18
Aiquen <aiqueneldar@gmail.com>
2014-12-14 02:41:58 +0100

    Work on issue #25
    Implemented Thaumcraft API to make our block/items give off aspects. Not
    sure what aspects they should give so I just took arbitary values and
    assigned them. Mod works with/without Thaumcraft. But needs Thaumcraft
    API to build if implemented. Won't change readme.md until we decide and
    implement into master.

--- src/main/java/se/luppii/ladders/lib/References.java
+++ src/main/java/se/luppii/ladders/lib/References.java
@@ -20,4 +20,11 @@ public class References {
 	public static final String MOD_HELP_ROPES_NAME = "Luppii's Ladders Compatible: Ropes+";
 
 	public static final String MOD_HELP_ROPES_VERSION = "0.0.1";
+	
+	// Helper mod for Thaumcraft mod
+	public static final String MOD_HELP_THAUMCRAFT_ID = "LLadders|Thaumcraft";
+
+	public static final String MOD_HELP_THAUMCRAFT_NAME = "Luppii's Ladders Compatible: Thaumcraft 4";
+
+	public static final String MOD_HELP_THAUMCRAFT_VERSION = "0.0.1";
 }
---
+++ src/main/java/se/luppii/ladders/modhelper/thaumcraft/Thaumcraft.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package se.luppii.ladders.modhelper.thaumcraft;
+
+import se.luppii.ladders.LLadders;
+import se.luppii.ladders.lib.Config;
+import se.luppii.ladders.lib.References;
+import se.luppii.ladders.modhelper.IExtension;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.aspects.AspectList;
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.common.Optional;
+
+
+/**
+ * @author aiquen
+ *
+ */
+
+public class Thaumcraft implements IExtension {
+
+	@Override
+	@Optional.Method(modid = "Thaumcraft")
+	public void load() {
+		if (Config.useThaumcraft.getBoolean(true)) {
+			// Facing directions
+			int[] direction =  {0, 1, 2, 3};
+			
+			// Sturdy ladders
+			thaumcraft.api.ThaumcraftApi.registerObjectTag(new ItemStack(LLadders.blockSturdyLadder), direction, (new AspectList()).add(Aspect.TOOL, 1).add(Aspect.METAL, 2));
+			
+			// Rope ladders
+			thaumcraft.api.ThaumcraftApi.registerObjectTag(new ItemStack(LLadders.blockRopeLadder), direction,  new AspectList().add(Aspect.TOOL, 1).add(Aspect.TREE, 2));
+			
+			//Vine ladders
+			thaumcraft.api.ThaumcraftApi.registerObjectTag(new ItemStack(LLadders.blockVineLadder), direction,  new AspectList().add(Aspect.TOOL, 1).add(Aspect.PLANT, 2));
+			
+			//Ladder dispenser
+			thaumcraft.api.ThaumcraftApi.registerObjectTag(new ItemStack(LLadders.blockLadderDispenser), direction, new AspectList().add(Aspect.MECHANISM, 2).add(Aspect.MOTION, 1).add(Aspect.METAL, 4).add(Aspect.ENERGY, 1));
+			
+			//Bride builder
+			thaumcraft.api.ThaumcraftApi.registerObjectTag(new ItemStack(LLadders.blockBridgeBuilder), new int[]{0, 1, 2, 3, 4, 5}, new AspectList().add(Aspect.MECHANISM, 2).add(Aspect.MOTION, 1).add(Aspect.METAL, 4).add(Aspect.ENERGY, 1));
+		}
+	}
+
+}
